d5694ec98d82322e85ae837f9692d2f514a48820
YuriySga <ess1928@yandex.ru>
2021-05-26 00:10:53 +0400

    doc: add jsdoc in userRepo

--- src/resources/users/user.memory.repository.js
+++ src/resources/users/user.memory.repository.js
@@ -1,16 +1,41 @@
+/**
+ * A module work with user repositories
+ * @module userRepo
+ */
+
 const DB = require("../../common/inMemoryDb");
 const taskService = require("../tasks/task.service");
 const User = require("./user.model");
 
+/**
+ * Returns the array of all Users
+ * @returns {User[]}
+ */
 const getAll = async () => DB.userDB;
+
+/**
+ * Returns the User on id
+ * @param {string} id
+ * @returns {User}
+ */
 const get = async (id) => DB.userDB.find(user=>user.id === id);
 
+/**
+ * Create the User in data base
+ * @param {User} user
+ * @returns {get} Return get function
+ */
 const create = async (user) => {
   const newUser = new User(user);
   DB.userDB.push(newUser);
   return get(newUser.id);
 };
 
+/**
+ * Delete User on id
+ * @param {string} id
+ * @returns {number} return status code
+ */
 const del = async (id) => {
   const userIndex = DB.userDB.findIndex((user) => user.id === id);
   if (userIndex !== -1) {
@@ -22,6 +47,12 @@ const del = async (id) => {
   return 404;  
 };
 
+
+/** update
+ * Update User in data base
+ * @param {User} user
+ * @returns {undefined | create} Return undefined or "create" user function
+ */
 const update = async (user) => del(user.id)
   .then(status => {    
     if (status === 204) {
--- src/resources/users/user.model.js
+++ src/resources/users/user.model.js
@@ -1,6 +1,14 @@
 
 const { randomUUID } = require('crypto');
 
+/** Class create user. */
+/**
+ * Create user.
+ * @param {string | undefined} id - The id value, if undefined: set random id .
+ * @param {string} name - The name value.
+ * @param {string} login - The login value.
+ * @param {string} password - The password value. 
+ */
 class User {
   constructor({
     id = randomUUID(),
@@ -14,6 +22,11 @@ class User {
     this.password = password;
   }
 
+  /**
+   * Return user data without password.
+   * @param {User} user - The object user.
+   * @return {User} A User object without password field.
+   */
   static toResponse(user) {
     const { id, name, login } = user;
     return { id, name, login };
