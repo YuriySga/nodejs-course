20aa72af02d986f33cd8bd502bdf74fefdad7600
YuriySga <ess1928@yandex.ru>
2021-06-22 20:51:25 +0400

    feat: do working test to docker

--- src/common/ormconfig.ts
+++ src/common/ormconfig.ts
@@ -1,35 +1,19 @@
-/* eslint-disable @typescript-eslint/naming-convention */
-// import dotenv from 'dotenv';
 import { ConnectionOptions } from 'typeorm';
-import dotenv from 'dotenv';
-import path from 'path';
+
 import { User } from '../entities/User';
 import { Board } from '../entities/Board';
 import { BoardColumn } from '../entities/BoardColumn';
 import { BoardTask } from '../entities/BoardTask';
 
-dotenv.config({
-  path: path.join(__dirname, '../../.env')
-});
-// import path, { dirname } from 'path';
-// import { fileURLToPath } from 'url';
-
-/* const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename); */
-
-/* dotenv.config({
-    path: path.join(__dirname, '../../.env')
-}); */
-
 export const config = {
     type: 'postgres',
     synchronize: true,
     logging: true,
-    host: process.env['POSTGRES_HOST'],
-    port: process.env['POSTGRES_PORT'],
-    username: process.env['POSTGRES_USER'],
-    password: process.env['POSTGRES_PASSWORD'],
-    database: process.env['POSTGRES_DB'],   
+    host: process.env['P_HOST'],
+    port: process.env['P_PORT'],
+    username: process.env['P_USER'],
+    password: process.env['P_PASSWORD'],
+    database: process.env['P_DB'],   
     autoReconnect: true,
     reconnectTries: 10,
     reconnectInterval: 1000,
