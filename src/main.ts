1de9a7c5b851c37d7c9ee8bfb34032a95e793150
YuriySga <ess1928@yandex.ru>
2021-07-10 18:22:08 +0400

    feat: inplement swagger

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,36 @@
 import { NestFactory } from '@nestjs/core';
 import { FastifyAdapter } from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   if (String(process.env['USE_FASTIFY']) === 'false') {
     const app = await NestFactory.create(AppModule);
+
+    const config = new DocumentBuilder()
+      .setTitle('Nest swagger api')
+      .setDescription('The nest API description')
+      .setVersion('1.0')
+      .addTag('nest')
+      .build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, document);
+
     await app.listen(process.env['PORT']);
     console.log(`Application is running on: ${await app.getUrl()}`);
     console.log(`Application is running with Express`);
   } else {
     const app = await NestFactory.create(AppModule, new FastifyAdapter());
+
+    const config = new DocumentBuilder()
+      .setTitle('Nest swagger api')
+      .setDescription('The nest API description')
+      .setVersion('1.0')
+      .addTag('nest')
+      .build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, document);
+
     await app.listen(process.env['PORT']);
     console.log(`Application is running on: ${await app.getUrl()}`);
     console.log(`Application is running with Fastify`);
